151c4c8352cbb3c803b5290c44afbdb45f17fb95
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2020-01-14 15:10:45 -0500

    Create AuthService + register & login functions.

---
+++ social-dev/social-dev/Services/AuthService.swift
@@ -0,0 +1,37 @@
+//
+//  AuthService.swift
+//  social-dev
+//
+//  Created by Kilyan SOCKALINGUM on 14/01/2020.
+//  Copyright © 2020 Kilyan SOCKALINGUM. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthService {
+    static let instance = AuthService()
+    
+    func registerUser(withEmail email: String, andPassword pwd: String, userCreationComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
+        Auth.auth().createUser(withEmail: email, password: pwd) { (userResult, error) in
+            guard let user = userResult else {
+                userCreationComplete(false, error)
+                return
+            }
+            let userData = ["provider": user.additionalUserInfo?.providerID as Any, "email": user.user.email!]
+            DataService.instance.createDBUser(uid: user.user.uid, userData: userData)
+            userCreationComplete(true, nil)
+        }
+    }
+    
+    func loginUser(withEmail email: String, andPassword pwd: String, loginComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
+        Auth.auth().signIn(withEmail: email, password: pwd) { (userResult, error) in
+            guard let user = userResult else {
+                loginComplete(false, error)
+                return
+            }
+            
+            loginComplete(true, nil)
+        }
+    }
+}
